0151deda3e19f414392d8dc4c13ab322c440a173
David H <davidhorph@hotmail.com>
2018-07-08 16:28:26 -0500

    viewscreen changes...
    goes from number to text question.

--- RecallRank.js
+++ RecallRank.js
@@ -3,6 +3,7 @@
 //From HTML screen.
 let timer = document.querySelector(".timer"); //timer element.
 let viewscreen = document.querySelector(".viewscreen"); //viewscreen element;
+let button = document.querySelector("button"); //button.
 
 let RandInteger = Math.floor(Math.random()*10)+1; //Generate Random Integer.
 let Stringnum = ""; //This is where we add the number to grow our string.
@@ -14,8 +15,8 @@ let clock = setInterval(function() {
     count +=1;
     if(count == 10) {
         clearInterval(1);
-        viewscreen.classList.add("HideElements");
-        viewscreen.classList.remove("HideElements");
+        button.classList.remove("HideElements");
+        viewscreen.textContent = "What was the number you just saw?"
     } else {
         count;
         button.classList.add("HideElements");
