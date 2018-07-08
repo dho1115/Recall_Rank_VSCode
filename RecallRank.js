19fac0fad6d7380605b43edc82984a2568b71dfd
David H <davidhorph@hotmail.com>
2018-07-08 16:06:38 -0500

    Start Javascript assigning variables.

--- RecallRank.js
+++ RecallRank.js
@@ -1,2 +1,11 @@
 
 
+//From HTML screen.
+let timer = document.querySelector(".timer"); //timer element.
+let viewscreen = document.querySelector(".viewsreen"); //viewscreen element;
+
+let RandInteger = Math.floor(Math.random()*10)+1; //Generate Random Integer.
+let Stringnum = ""; //This is where we add the number to grow our string.
+
+viewscreen.textContent = RandInteger;
+
