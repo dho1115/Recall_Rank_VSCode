ac6dad10d325a5e774e66aca6c99e8ecada28a59
David H <davidhorph@hotmail.com>
2018-07-08 16:21:42 -0500

    ...

--- RecallRank.js
+++ RecallRank.js
@@ -2,10 +2,24 @@
 
 //From HTML screen.
 let timer = document.querySelector(".timer"); //timer element.
-let viewscreen = document.querySelector(".viewsreen"); //viewscreen element;
+let viewscreen = document.querySelector(".viewscreen"); //viewscreen element;
 
 let RandInteger = Math.floor(Math.random()*10)+1; //Generate Random Integer.
 let Stringnum = ""; //This is where we add the number to grow our string.
+let count = 1; //This will go inside the set interval and stop at a pre-defined number.
 
 viewscreen.textContent = RandInteger;
+
+let clock = setInterval(function() {
+    count +=1;
+    if(count == 10) {
+        clearInterval(1);
+        viewscreen.classList.add("HideElements");
+        viewscreen.classList.remove("HideElements");
+    } else {
+        count;
+        button.classList.add("HideElements");
+    }
+    timer.textContent = count;
+},1000)
 
