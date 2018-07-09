96f231dc715efba7d3b169afb9b54a600e988e83
David H <davidhorph@hotmail.com>
2018-07-08 19:56:09 -0500

    ...

--- RecallRank.js
+++ RecallRank.js
@@ -4,23 +4,53 @@
 let timer = document.querySelector(".timer"); //timer element.
 let viewscreen = document.querySelector(".viewscreen"); //viewscreen element;
 let button = document.querySelector("button"); //button.
-
+let guessBox = document.querySelector(".guessBox");
+let recallrank = document.querySelector("recallrank");
+let recallscore = 0; //This is your recall score.
+let response = document.querySelector("response"); //reveals whether you are right or wrong.
 let RandInteger = Math.floor(Math.random()*10)+1; //Generate Random Integer.
 let Stringnum = ""; //This is where we add the number to grow our string.
-let count = 1; //This will go inside the set interval and stop at a pre-defined number.
+let count = 0; //This will go inside the set interval and act as TIMER.
 
-viewscreen.textContent = RandInteger;
+///////////////Functions Needed For The Program///////////////
+function HideButtonAndInputAtStart() {  //Hides Button and Input box.
+    button.classList.add("HideElements");
+    guessBox.classList.add("HideElements");
+} 
+function ShowButtonInputAndTextChange() {  //Reveals Button and Input box.
+    button.classList.remove("HideElements");
+    guessBox.classList.remove("HideElements");
+    viewscreen.textContent = "What was the number you just saw?"
+} 
+function CorrectGuess() {  //If the guess is right.
+    recallscore +=5;
+    recallrank.textContent = recallscore;
+    response.textContent = "CORRECT!!!"
+    return response.textContent;
+} 
+function WrongGuess() {  //If the guess is wrong.
+    response.textContent = "Sorry. That's wrong."
+    return response.textContent;
+}
+function RevealNumber() {  //Reveals the number on the viewscreen.
+    RandInteger = Math.floor(Math.random()*10)+1;
+    RandInteger > 9 ? RandInteger -=2 : RandInteger;    
+    Stringnum += RandInteger;
+    viewscreen.textContent = Number(Stringnum);    
+    return viewscreen.textContent;
+}
+/////////////////////////////////////////////////////////////
+
+RevealNumber();
 
 let clock = setInterval(function() {
     count +=1;
-    if(count == 10) {
+    if(count == 9) {
         clearInterval(1);
-        button.classList.remove("HideElements");
-        viewscreen.textContent = "What was the number you just saw?"
+        ShowButtonInputAndTextChange();        
     } else {
-        count;
-        button.classList.add("HideElements");
+        HideButtonAndInputAtStart();
+        timer.textContent = 8-count;
     }
-    timer.textContent = count;
 },1000)
 
